c2046982dc9cf7ac006ef4f93b24f023ea1c2c27
theemperorabdoalleader <theemperorabdoalleader@gmail.com>
2026-05-12 04:19:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("@whiskeysockets/baileys")
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason
+} = require("@whiskeysockets/baileys")
+
+const P = require("pino")
 
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("session")
 
   const sock = makeWASocket({
     auth: state,
-    printQRInTerminal: true
+    logger: P({ level: "silent" }),
+    printQRInTerminal: true // أول مرة بس عشان تعمل تسجيل
   })
 
   sock.ev.on("creds.update", saveCreds)
 
   sock.ev.on("connection.update", (update) => {
-    const { connection, lastDisconnect } = update
+    const { connection, lastDisconnect, qr } = update
+
+    if (qr) {
+      console.log("📌 امسح الـ QR من واتساب:")
+      console.log(qr)
+    }
 
     if (connection === "open") {
-      console.log("✅ البوت شغال")
+      console.log("✅ البوت اشتغل واتصل بنجاح")
     }
 
     if (connection === "close") {
       const shouldReconnect =
         lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut
 
-      if (shouldReconnect) startBot()
+      console.log("🔄 إعادة تشغيل البوت...", shouldReconnect)
+
+      if (shouldReconnect) {
+        startBot()
+      }
     }
   })
 }
