a5d4e7f2e450705042a868af36117d34060aa00d
theemperorabdoalleader <theemperorabdoalleader@gmail.com>
2026-05-13 05:57:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function startBot() {
 
   const sock = makeWASocket({
     auth: state,
-    printQRInTerminal: false
+    printQRInTerminal: true
   });
 
   sock.ev.on("creds.update", saveCreds);
