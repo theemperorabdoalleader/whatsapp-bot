b3cf08acb9600b5a410c9750eefb3e53d1aedeb7
theemperorabdoalleader <theemperorabdoalleader@gmail.com>
2026-05-13 04:24:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,29 +10,29 @@ async function startBot() {
     logger: P({ level: "silent" })
   });
 
-  // حفظ السيشن
   sock.ev.on("creds.update", saveCreds);
 
   sock.ev.on("connection.update", (update) => {
     const { connection, qr } = update;
 
-    // عرض QR
     if (qr) {
       console.log("📌 امسح الكود ده:");
       qrcode.generate(qr, { small: true });
     }
 
-    // تم الاتصال
     if (connection === "open") {
       console.log("✅ البوت اشتغل واتصل بنجاح");
     }
 
-    // لو الاتصال اتقفل
     if (connection === "close") {
-      console.log("❌ الاتصال اتقفل... مستني إعادة تشغيل");
+      console.log("❌ الاتصال اتقفل... بنحاول تاني");
+
+      // 🔥 أهم سطر (إعادة المحاولة)
+      setTimeout(() => {
+        startBot();
+      }, 3000);
     }
   });
 }
 
-// تشغيل البوت
 startBot();
