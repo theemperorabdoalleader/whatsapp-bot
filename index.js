21133a24764087205b94e9983274884ade663199
theemperorabdoalleader <theemperorabdoalleader@gmail.com>
2026-05-13 04:32:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,26 @@ async function startBot() {
     logger: P({ level: "silent" })
   });
 
-  // حفظ السيشن
   sock.ev.on("creds.update", saveCreds);
 
-  // 🔥 كود الربط برقمك
-  if (!sock.authState.creds.registered) {
-    const phoneNumber = "201149182286"; // رقمك اتحط هنا
-
-    const code = await sock.requestPairingCode(phoneNumber);
-    console.log("📌 كود الربط:", code);
-  }
-
-  sock.ev.on("connection.update", (update) => {
+  sock.ev.on("connection.update", async (update) => {
     const { connection } = update;
+
+    // 🔥 أول ما الاتصال يبدأ
+    if (connection === "connecting") {
+      console.log("⏳ جاري الاتصال...");
+
+      if (!sock.authState.creds.registered) {
+        const phoneNumber = "201149182286"; // رقمك
+
+        try {
+          const code = await sock.requestPairingCode(phoneNumber);
+          console.log("📌 كود الربط:", code);
+        } catch (err) {
+          console.log("❌ حصل خطأ في الكود:", err.message);
+        }
+      }
+    }
 
     if (connection === "open") {
       console.log("✅ البوت اشتغل واتصل بنجاح");
